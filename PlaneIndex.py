10af121cbfe743f451b47e32684d4a035e6288b2
demon <1653223198@qq.com>
2022-06-22 17:42:02 +0800

    键盘事件的加入

--- PlaneIndex.py
+++ PlaneIndex.py
@@ -7,16 +7,43 @@ def main():
     创建一个窗口
     :return:
     '''
-    screen=pygame.display.set_mode((350,500))
-    #设定窗口大小
+    # 设定窗口大小
+    screen=pygame.display.set_mode((350,500),depth=32)
+
+    # 设定背景图片
     background=pygame.image.load('./feiji/背景.png')
-    #设定背景图片
+
+    # 设置窗口标题
     pygame.display.set_caption('双人飞机大战')
-    #设置窗口标题
+
     while True:
+
+        # 设定窗口背景居中显示
         screen.blit(background,(0,0))
-        #设定图片居中显示
+        #设定键盘事件
+        eventList=pygame.event.get()
+        for event in eventList:
+            if  event.type==QUIT:
+                print('退出游戏')
+                #退出程序
+                exit()
+                pass
+            elif event.type==KEYDOWN:
+                if event.key==K_a or event.key==K_LEFT:
+                    print('向左移动')
+                    pass
+                elif event.key==K_d or event.key==K_RIGHT:
+                    print('向右移动')
+                    pass
+                elif event.key==K_SPACE:
+                    print('发射子弹')
+                    pass
+
+        # 设定刷新窗口
         pygame.display.update()
-        #设定刷新窗口
+
     pass
-main()
+
+
+if __name__=='__main__':
+    main()
